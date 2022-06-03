86b7ff8a959bdd46a9f459bbdd271aae003e9ec0
Abood <sayyedabood69@gmail.com>
2022-06-03 20:01:39 +0530

    Setting up my backend server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,32 @@
-console.log("hello, World")
+//* IMPORT from packages
+const express = require('express');  // optional naming: express, you can name this anything
+// import 'package:express/express.dart';
+
+//* IMPORT from FILES
+const authRouter = require("./routes/auth")
+
+
+//* INIT
+const PORT = 3000;
+const app = express();
+
+//* MIDDLEWARE
+app.use(authRouter);
+
+// CREATING AN API
+// GET, PUT, POST, DELETE, UPDATE -> CRUD
+
+// https://<your-ip-address>/<your-path-name in .get>
+// app.get("/", (req, res) => {
+//     res.json({name: "Abdul raheem"})
+// })
+
+app.listen(PORT, () => {
+    console.log(`connected at port ${PORT}`);   // JS you use ` back comma and ${} for string interpolation
+});
+// If IP not specified then it will use default
+// LOCALHOST -> for some purposes android emulator doesnt support LOCALHOST
+// so for debugging purpose we are using IP 0.0.0.0 which means
+// it can be accessed from anywhere
+//
+// 2 ways to write call back functions in JS 'function () {}'  OR '() => {}'
