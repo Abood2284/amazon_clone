0ef12abf2960ef4815b7880a4290dfe9f35801e0
Abood <sayyedabood69@gmail.com>
2022-06-30 13:20:24 +0530

    Heroku pushing

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://192.168.0.12:3000';
+// String uri = 'http://192.168.0.12:3000';
+String uri = 'https://amazon-clone-abdul.herokuapp.com/';
 
 class GlobalVariables {
   // COLORS
